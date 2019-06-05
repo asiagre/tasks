e659e9031f97a29d2596b1a0f7370bc32df1687c
Joanna Zawiślak <asia.greczka@gmail.com>
2019-06-05 15:13:14 +0200

    Exercise_23.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
-@Api
 @RestController
 @RequestMapping("/v1/task")
 @CrossOrigin(origins = "*")
